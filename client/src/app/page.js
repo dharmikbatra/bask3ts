776f9d5f18f5c9efa514ff8758baa95246bcb7bf
dhbatra <dhbatra@cisco.com>
2023-12-09 13:38:11 +0530

    add notifications

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -5,6 +5,7 @@ import firstpage from "../app/public/firstpage.png"
 import Link from "next/link";
 import { useState } from 'react';
 // import Modal from '../components/Modal';
+import Mymodal from "../components/Mymodal";
 // import useModal from "./context/useModal";
 // import { GlobalContextProvider, useGlobalContext } from "./context/store";
 
@@ -15,7 +16,8 @@ export default function Home() {
   <div className="row">
     <div className="col-4">
     <Image src= {firstpage} alt=" " height="50%" width="50%"  />
-    
+
+    <Mymodal />
     </div>
     <div className="col-8">
     You don’t have to be a digital marketing pro to know how important a website is to modern business. As both a digital interface for delivering products and services and a vehicle for generating leads, your website needs to look good. If you want to deliver a smooth customer experience and look good while doing it, a web designer can help.
